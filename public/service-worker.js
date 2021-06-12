b86a6b4f1f5f24383f035f059bafa9f819e04f9a
Edita Pesa <editapesa@gmail.com>
2021-06-11 21:14:36 -0400

    add code to install and register the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,16 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
+//install & register the service worker
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add('/api/transactions'))
+    );
+
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
+
